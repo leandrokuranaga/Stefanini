b0d56287fb27a9d4cd71c8051d35d1510ee430da
Leandro Kuranaga <le.s.kuranaga@hotmail.com>
2024-01-14 22:33:38 -0300

    Update swagger-initializer.js

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -1,6 +1,6 @@
 window.onload = function() {
   window.ui = SwaggerUIBundle({
-    url: "swagger.json",
+    url: "/swagger.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
